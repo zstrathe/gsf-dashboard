986fb6c80b6ae6a8c2b34f38cb415775af724a43
Zack Strathe <zstrathe@outlook.com>
2024-02-20 00:53:51 -0600

    update dash app with PDF download button and updated formatting

--- src/dash_app/app.py
+++ src/dash_app/app.py
@@ -1,9 +1,10 @@
 import base64
 from io import BytesIO
+import cairosvg
 import matplotlib
 matplotlib.use("Agg")
 from matplotlib import pyplot as plt
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, no_update
 import dash_bootstrap_components as dbc
 from pandas import to_datetime
 
@@ -32,55 +33,63 @@ def serve_layout():
         [
             html.Div(print("Loading page..."), id="page-load-trigger", hidden=True),
             dbc.Row(
-                [dbc.Col([html.H1("GSF Reports")], style={"textAlign": "center"})],
+                [dbc.Col([html.H1("GSF Reports Dashboard")], style={"textAlign": "center"})],
                 justify="center",
             ),
             dbc.Row(
                 [
                     dbc.Col(
                         [
-                            html.P("Date selection:      "),
+                            dbc.Stack(
+                                [
+                                html.P("Date selection:",
+                                       style={'padding-top': '15px'}),
+                                dcc.DatePickerSingle(
+                                    id="dropdown-date-selection",
+                                    placeholder="Select a date",
+                                )
+                            ], direction='horizontal', gap=3, className='float-end')
                         ],
-                        style={"textAlign": "right", "padding-top": 13},
-                        width={"size": 2},
-                    ),
+                    style={'border-style': 'none', 'textAlign': 'right'}, width='auto'),
+
                     dbc.Col(
                         [
-                            dcc.DatePickerSingle(
-                                id="dropdown-date-selection",
-                                placeholder="Select a date",
+                            dbc.Stack(
+                                [
+                                html.Div("Report selection:", 
+                                         style={'padding-top': '7px'}),
+                                dcc.Dropdown(
+                                    REPORT_OPTIONS,
+                                    id="dropdown-report-selection",
+                                    placeholder="Select a report",
+                                    clearable=True,
+                                    style={'width': '275px', 'height': '47px', 'padding-top': '4px'}
+                                )
+                                ], direction='horizontal', gap=3, className='float-start'
+                            ),
+
+                            dbc.Stack(
+                                [
+                                    html.Button(
+                                        'Download as PDF',
+                                        id='download_button',
+                                        n_clicks=0, 
+                                        hidden=True,
+                                        style={'margin-left': '75px', 'margin-top': '12px'}
+                                        ),
+                                    dcc.Loading(
+                                        children=dcc.Download(id='download'),
+                                        style={"margin-left": "45px", "margin-top": "12px"},
+                                        type='circle',
+                                        ),
+                                    html.Div(id='hidden-connector-dl', hidden=True)
+                                ], direction='horizontal', gap=3, className='float-end'
                             )
                         ],
-                        style={"textAlign": "left"},
-                        width={"size": 2},
-                    ),
-                ],
-                justify="center",
+                    style={'border-style': 'none', 'white-space': 'nowrap', }, width='auto')
+                ], justify='center'
             ),
-            dbc.Row(
-                [
-                    dbc.Col(
-                        [
-                            html.P("Report selection:      "),
-                        ],
-                        style={"textAlign": "right", "padding-top": 5},
-                        width=2,
-                    ),
-                    dbc.Col(
-                        [
-                            dcc.Dropdown(
-                                REPORT_OPTIONS,
-                                id="dropdown-report-selection",
-                                placeholder="Select a report",
-                                clearable=True,
-                            )
-                        ],
-                        style={"textAlign": "left"},
-                        width=2,
-                    ),
-                ],
-                justify="center",
-            ),
+                   
             dbc.Row(
                 [
                     dbc.Col(
@@ -92,7 +101,7 @@ def serve_layout():
                                 children=html.Img(id="graph-content"),
                                 type="default",
                                 fullscreen=False,
-                                style={"textAlign": "top"},
+                                style={'margin-top': '50px'},
                             ),
                         ],
                         style={"textAlign": "center"},
@@ -151,19 +160,20 @@ def clear_report_output_when_generating_new(selected_date, selected_report):
 def update_graph(options):
 
     if options is None:
-        return None
+        return no_update
+    elif None in options:
+        return no_update
 
     select_date = to_datetime(options[0])
     select_report = options[1]
 
     # get and return cached image for report if one exists
     cache_options = f'{select_date} {select_report}'
-    #cache_fig = cache.get(cache_options)
     cache_data = cache.get_cache_item(cache_options)
     if cache_data is not None:
-        print('Found cached report!')
-        cache_data = cache_data.decode("ascii")
-        cache_fig = f"data:image/png;base64,{cache_data}"
+        print('DISPLAY: Found cached report!')
+        cache_data = base64.b64encode(cache_data).decode()
+        cache_fig = f"data:image/svg+xml;base64,{cache_data}"
         return cache_fig
 
     # generate figure depending on report selection
@@ -183,27 +193,79 @@ def update_graph(options):
         case "Expense Report":
             fig = ExpenseGridReport(report_date=select_date, save_output=False).run()
         case _:
-            # Default case, return nothing if no report option selected
-            return None
+            # Default case, raise Exception
+            # since report options are hardcoded, this case should never occur
+            raise Exception(
+                f'ERROR: report option specified: {select_report}, but no report generation method is defined!'
+                )
 
     # Save it to a temporary buffer.
     buf = BytesIO()
-    fig.savefig(buf, format="png", bbox_inches="tight")
-    # Embed the result in the html output.
-    bytes_data = base64.b64encode(buf.getbuffer())
-    fig_data = bytes_data.decode("ascii")
-    fig_img = f"data:image/png;base64,{fig_data}"
+    fig.savefig(buf, format="svg", bbox_inches="tight")
     # close the figure to release it from memory
     plt.close(fig)
+   
+    # Embed the result in the html output.
+    bytes_data = buf.getbuffer()
+    fig_data = base64.b64encode(bytes_data).decode()
+    fig_img = f"data:image/svg+xml;base64,{fig_data}"
 
     # add report to the report cache
     cache.add_cache_item(cache_options, bytes_data)
 
     return fig_img
 
+# callback to unhide download button if a report is generated 
+@app.callback(Output("download_button", "hidden"),
+              Input('graph-content', 'src'),
+              prevent_initial_call=True)
+def unhide_download_button(fig_img_data):
+    if fig_img_data is not None:
+        return False # return False to unset 'hidden' property of download button
+    else:
+        return True
+
+# callback for initial processing of download button, so that loading animation is only triggered when
+# the button is clicked, and not when dropdown options are changed
+# use 'hidden-connector-dl' as an intermediate "container" for dropdown values to pass to the generate_pdf() 
+# function. 
+@app.callback([Output("hidden-connector-dl", "children"),
+               Output('download_button', 'n_clicks', allow_duplicate=True)],
+              [Input('download_button', 'n_clicks'),
+               Input("dropdown-date-selection", "date"),
+               Input("dropdown-report-selection", "value")],
+               prevent_initial_call=True)
+def trigger_download(n_clicks, date_selection, report_selection):
+    if n_clicks == 0:
+        return no_update
+    else:
+        return [[date_selection, report_selection], 0] # return 0 to reset n_clicks to 0
+
+# callback to download report when download button is clicked
+@app.callback(Output('download', 'data'),
+              Input("hidden-connector-dl", "children"),
+              prevent_initial_call=True)
+def generate_pdf(report_options):
+    if report_options is None:
+        return no_update
+
+    select_date = to_datetime(report_options[0])
+    select_report = report_options[1]
+
+    # get and return cached image for report if one exists
+    cache_options = f'{select_date} {select_report}'
+    cache_data = cache.get_cache_item(cache_options)
+    if cache_data is None:
+        return no_update
+      
+    def write_pdf(bytes_io):
+        bytes_io.write(cairosvg.svg2pdf(bytestring=cache_data))
+    
+    return dcc.send_bytes(write_pdf, f'{select_date.strftime("%Y-%m-%d")} {select_report}.pdf')
+
 
 # on app load, check if the sqlite db file in 'backup' folder (intended for cloud file store) 
-# is different from the current db file in the container
+# is newer than the current db file in the container
 # this is a stupid workaround required due to using a sqlite db rather than something better suited
 # for cloud usage...because otherwise the db file wont persist through container restarts
 load_sqlite_db()
